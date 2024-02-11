326e8d0696a09bb9177f8e878edcf5cc75609bff
DallasMacD <82773277+DallasMacD@users.noreply.github.com>
2024-02-11 14:34:16 -0600

    Update Start_Streamlit.py

--- Start_Streamlit.py
+++ Start_Streamlit.py
@@ -8,7 +8,7 @@ cwd = os.path.dirname(__file__)
 os.chdir(cwd)
 
 # Run the ncp_case_comparison_using_streamlit.py script from a command prompt window
-subprocess.Popen('streamlit run ' + '"' + cwd + '\\0_🏠_Home.py" --theme.base="light" --theme.primaryColor="#0079C1" --theme.backgroundColor="#FFFFFF" --theme.secondaryBackgroundColor="#E5F5FD" --theme.textColor="#262730" --theme.font="sans serif" --server.headless true', creationflags=subprocess.CREATE_NEW_CONSOLE)
+subprocess.Popen('streamlit run ' + '"' + cwd + '\\0_🏠_Home.py" --theme.base="light" --theme.primaryColor="#0079C1" --theme.backgroundColor="#FFFFFF" --theme.secondaryBackgroundColor="#E5F5FD" --theme.textColor="#262730" --theme.font="sans serif" --server.headless true', shell=True)
 
 # Get the IP address of your computer
 host_name = socket.gethostname()
