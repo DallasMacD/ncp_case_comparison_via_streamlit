81922ef74a1f53fbfe37e55be62e9fb6bbce2026
DallasMacD <82773277+DallasMacD@users.noreply.github.com>
2024-02-11 16:20:52 -0600

    Update data_module.py

--- data/data_module.py
+++ data/data_module.py
@@ -518,7 +518,7 @@ def extract_csv_metrics(directory: str, case_name: str, csv_metrics_dict: dict):
 		csv_metrics_df: A DataFrame containing the metrics specified in csv_metrics_dict, with Hour as the index and Case -> Metric Name -> Agent as the multiindex column levels
 	
 	'''
-	st.write(csv_metrics_dict)
+	st.write(directory)
 	# Create an empty placeholder DataFrame to store the csv metrics specified in csv_metrics_dict
 	csv_metrics_df = pd.DataFrame()
 	# Read each file specified in the csv_metrics_dict and store the results in the csv_metrics_df DataFrame
